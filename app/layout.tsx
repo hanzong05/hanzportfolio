f327dbf258ad8ef315a73ac287117a54568a34fa
Hanz Pillerva <hanzpillerva@example.com>
2025-12-15 10:40:51 +0800

    update fav icom

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,9 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
         <script
           dangerouslySetInnerHTML={{
             __html: `
